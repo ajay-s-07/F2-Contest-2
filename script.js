0b7ed8f3b8e400fb204f2e0ec4c1f17e05fd0097
Ajay Sarkate <115990815+ajay-s-07@users.noreply.github.com>
2023-02-18 18:08:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,92 @@
-let array = [];
 
-let button1 = document.getElementById("btn");
+let arr=[];
+ let d=document.querySelector('.t');
+let a=document.querySelector(".issue");
 
-function issueBooks(){
-    let bookName = document.getElementById("bookName").value;
-    let user = document.getElementById("user").value;
-    let id = array.length+1; 
-    let time = new Date().toLocaleString();
-    let status  = "not returned";
-
-    let obj = {id,bookName,user,time,status};
-    array.push(obj);
-    displayData();
+let tbl=document.querySelector('tbody');
+let count=0;
+function fun(){
+  let tr=document.createElement('tr');
+let td1=tr.appendChild(document.createElement('td'));
+let td2=tr.appendChild(document.createElement('td'));
+let td3=tr.appendChild(document.createElement('td'));
+let td4=tr.appendChild(document.createElement('td'));
+let td5=tr.appendChild(document.createElement('td'));
+let date=new Date().toLocaleString();
+// alert(date);
+     let name=document.querySelector(".name").value;
+     let to=document.querySelector(".issued").value;
+     let st=document.createElement('span');
+     st.classList.add('red');
+     st.innerHTML="Not Returned"
+    //  let status=st;
+     let id=arr.length+1;
+     let obj={id,name,to,date,st};
+ arr.push(obj);
+   td2.innerHTML=name;
+   td3.innerHTML=to;
+   td1.innerHTML=id;
+   td4.innerHTML=date;
+   td5.appendChild(st);
+   td5.innerHTML+=' <a onclick="fun2('+id+')""><span class="material-icons md-36">edit_square</span></a>';
+  
+  if(!name=="" && !to=="")
+   tbl.append(tr);
+ name="";
+ to="";
 }
-button1.addEventListener('click', issueBooks);
+function fun2(id){
+  let st1=document.createElement('span');
+  let curr=arr[id-1].st;
+  // console.log(curr.classList);
+  
+  if(curr.innerHTML.includes('Not')){
+    let st1=document.createElement('span');
+    st1.classList.add('green');
+    st1.innerHTML="Returned"
+    arr[id-1].st=st1;
+    console.log(st1);
+  }
+  else{
+    let st1=document.createElement('span');
+    st1.classList.add('red');
+    st1.innerHTML="Not Returned"
+    arr[id-1].st=st1;
+  }
+  
+  rander();
 
-let table1 = document.getElementsByTagName("table");
-let body1 = document.getElementById("body");
-function displayData(){
-    let tr = document.createElement('tr');
-
-    let td1 = tr.appendChild(document.createElement('td'));
-    let td2 = tr.appendChild(document.createElement('td'));
-    let td3 = tr.appendChild(document.createElement('td'));
-    let td4 = tr.appendChild(document.createElement('td'));
-    let td5 = tr.appendChild(document.createElement('td'));
-
-    let book = array[array.length-1];
-    td1.innerText = book.id;
-    td2.innerText = book.bookName;
-    td3.innerText = book.user;
-    td4.innerText = book.time;
-    td5.innerText = book.status;
-
-    
-
-    if(!book.bookName == "" && !book.user == "")    
-        body1.appendChild(tr);
 }
+function rander(){
 
-// function checkStatus(event, id){
-//     const statusCell = event.target;
-//     const book = array.find(book => book.id === id);
-//     book.status = book.status === 'returned' ? 'not returned' : 'returned';
-//     statusCell.classList.toggle('issued');
-//     statusCell.classList.toggle('not-returned');
-//     statusCell.textContent = book.status;
-// }
+  tbl.innerHTML='';
+  arr.forEach(ele => {
+    let tr=document.createElement('tr');
+    let td1=tr.appendChild(document.createElement('td'));
+    let td2=tr.appendChild(document.createElement('td'));
+    let td3=tr.appendChild(document.createElement('td'));
+    let td4=tr.appendChild(document.createElement('td'));
+    let td5=tr.appendChild(document.createElement('td'));
+
+
+         let name=ele.name;
+         let to=ele.to;
+         let status=ele.st;
+         let id=ele.id;
+         let date=ele.date;
+         td1.innerHTML=id;
+         td2.innerHTML=name;
+       td3.innerHTML=to;
+       td4.innerHTML=date;
+       td5.appendChild(status);
+       td5.innerHTML+=' <a onclick="fun2('+id+')"><span class="material-icons md-36">edit_square</span></a>';
+       tbl.append(tr);
+  });
+
+}
+a.addEventListener('click',fun);
+// let b=document.querySelector('a');
+function f(){
+alert("hi");
+}
+// b.addEventListener('click',f);
